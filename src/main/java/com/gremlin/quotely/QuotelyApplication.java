045a24c44e59d857faf3d7e6bc37bccd97bcc545
Valentin Caloean <vcaloean@gmail.com>
2021-01-18 21:40:24 -0500

    Init commit

---
+++ src/main/java/com/gremlin/quotely/QuotelyApplication.java
@@ -0,0 +1,13 @@
+package com.gremlin.quotely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotelyApplication.class, args);
+	}
+
+}
